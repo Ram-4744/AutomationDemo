227288b92a2f378e3c6052816c9cb0894123399b
Ram <ramakrishna.balekai@gmail.com>
2025-01-29 13:03:07 +0530

    added code in reversstring

--- Demo/src/test/java/testPrograms/bubbleSort1.java
+++ Demo/src/test/java/testPrograms/bubbleSort1.java
@@ -1,9 +1,10 @@
 package testPrograms;
 
-public class bubbleSort {
+public class bubbleSort1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("hello world");
 
 	}
 
--- Demo/src/test/java/testPrograms/reversString.java
+++ Demo/src/test/java/testPrograms/reversString.java
@@ -4,8 +4,21 @@ public class reversString {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world");
 
+		String s = "Geeks";
+		String r = "";
+		char ch;
+
+		for (int i = 0; i < s.length(); i++) {
+
+			// extracts each character
+			ch = s.charAt(i);
+
+			// adds each character in
+			// front of the existing string
+			r = ch + r;
+		}
+
+		System.out.println(r);
 	}
-
 }
